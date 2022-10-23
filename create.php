1bc1a3e07de3117d8b34e9daca11f8f37fb87ceb
Arthur Canuto <99217088+arthurcanuto@users.noreply.github.com>
2022-10-23 20:37:11 -0300

    Update create.php

--- create.php
+++ create.php
@@ -2,11 +2,18 @@
 session_start();
 require 'conexaobd.php';
 
+function clear($input) {
+    global $connect;
+    $var = mysqli_escape_string($connect, $input);
+    $var = htmlspecialchars($var);
+    return $var;
+}
+
 if(isset($_POST['btn-cadastrar'])):
-$nome = mysqli_escape_string($connect, $_POST['nome']);
-$sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-$email = mysqli_escape_string($connect, $_POST['email']);
-$idade = mysqli_escape_string($connect, $_POST['idade']);
+$nome = clear($connect, $_POST['nome']);
+$sobrenome = clear($connect, $_POST['sobrenome']);
+$email = clear($connect, $_POST['email']);
+$idade = clear($connect, $_POST['idade']);
 
 $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')";
 
