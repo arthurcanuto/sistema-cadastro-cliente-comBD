d91ab67a8efae439fca60774e6f31b9829e1c365
Arthur Canuto <99217088+arthurcanuto@users.noreply.github.com>
2022-10-24 08:23:35 -0300

    Update create.php

--- create.php
+++ create.php
@@ -4,18 +4,20 @@ require 'conexaobd.php';
 
 function clear($input) {
     global $connect;
+
     $var = mysqli_escape_string($connect, $input);
     $var = htmlspecialchars($var);
     return $var;
 }
 
 if(isset($_POST['btn-cadastrar'])):
-$nome = clear($connect, $_POST['nome']);
-$sobrenome = clear($connect, $_POST['sobrenome']);
-$email = clear($connect, $_POST['email']);
-$idade = clear($connect, $_POST['idade']);
+$nome = clear($_POST['nome']);
+$sobrenome = clear($_POST['sobrenome']);
+$email = clear($_POST['email']);
+$idade = clear($_POST['idade']);
 
 $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')";
+
 
     if(mysqli_query($connect, $sql)):
         $_SESSION['mensagem'] = "Cadastrado com sucesso!";
