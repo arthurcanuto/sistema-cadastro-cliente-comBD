1b64979cc86ecb3d6d85f54ac1edd3b731de16f2
Arthur Canuto <99217088+arthurcanuto@users.noreply.github.com>
2022-10-24 11:00:12 -0300

    Update editar.php

--- editar.php
+++ editar.php
@@ -1,11 +1,12 @@
 <?php
-include_once'conexaobd.php';
-include 'header.php';
+include_once 'conexaobd.php';
+include_once 'header.php';
 
 if(isset($_GET['IdUsuario'])):
+  
     $id = mysqli_escape_string($connect, $_GET['IdUsuario']);
 
-    $sql = "SELECT * FROM clientes WHERE id = '$id'";
+    $sql = "SELECT * FROM clientes WHERE IdUsuario = '$id'";
     $resultado = mysqli_query($connect, $sql);
     $dados = mysqli_fetch_array($resultado);
 endif;
@@ -17,20 +18,20 @@ endif;
         <form action="atualizar.php" method="POST">
             <input type = "hidden" name= "IdUsuario" value = "<?php echo $dados['IdUsuario']; ?>">
             <div class= "input-field col s12">
-                <input type="text" name="nome" id="nome" value="<?php echo $dados['nome']; ?>>
+                <input type="text" value="<?php echo $dados['nome']; ?>" name="nome" id="nome"  >
                 <label for="nome">Nome</label>
             </div>
             <div class= "input-field col s12">
-                <input type="text" name="nome" id="nome" <?php echo $dados['sobrenome']; ?>>
-                <label for="nome">Sobrenome</label>
+                <input type="text" name="sobrenome" id="sobrenome" value="<?php echo $dados['sobrenome']; ?>">
+                <label for="sobrenome">Sobrenome</label>
             </div>
             <div class= "input-field col s12">
-                <input type="text" name="nome" id="nome" <?php echo $dados['email']; ?>>
-                <label for="nome">Email</label>
+                <input type="text" name="email" id="email" value="<?php echo $dados['email']; ?>">
+                <label for="email">Email</label>
             </div>
             <div class= "input-field col s12">
-                <input type="text" name="nome" id="nome"<?php echo $dados['idade']; ?>>
-                <label for="nome">Idade</label>
+                <input type="text" name="idade" id="idade" value="<?php echo $dados['idade']; ?>" >
+                <label for="idade">Idade</label>
             </div>
             <button type="submit" name="btn-editar" class="btn grey"> Atualizar</button>
             <a href="index.php" type="submit" class="btn green"> Lista de Clientes</a>
